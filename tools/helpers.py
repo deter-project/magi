3b9f66a20a3d02cdf6ac925b76d4194e1db8d910
hemantku <vgh4uster@gmail.com>
2014-08-05 17:24:05 -0700

    Graph Tool

--- tools/helpers.py
+++ tools/helpers.py
@@ -53,3 +53,25 @@ def getDBConfigHost(experimentConfigFile=None, project=None, experiment=None):
     
     return "%s.%s.%s" % (dbdl['configHost'], expdl['eid'], expdl['pid'])
 
+def load_yaml_idl(agent_name,agent_procedure):
+    agent_aal = load_yaml(agent_procedure)
+    try:
+        tar = tarfile.open(agent_aal['agents'][agent_name]['path'])
+        for member in tar.getmembers():
+            if member.name.endswith('.idl'):
+                f = tar.extractfile(member)
+                content = f.read()
+                config = yaml.load(content)
+                return config
+    except IOError as e:
+        logging.critical("File not found: %s", str(e))
+        sys.exit(2)
+        
+def load_yaml(file_aal):
+   try:
+		f=open(file_aal)  
+		config=yaml.load(f)
+        	return config
+   except IOError as e:
+		logging.critical("File not found: %s", str(e))
+		sys.exit(2)
