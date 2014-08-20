5d47f7707c27fa84f428a26412e2ffb7a0bfe914
Prateek Jaipuria <jaipuria@isi.edu>
2014-08-20 16:49:00 -0700

    Exception handling. Default values added.

--- tools/magi_graph.py
+++ tools/magi_graph.py
@@ -17,13 +17,13 @@ if __name__ == '__main__':
                                                     
     optparser.add_option("-e", "--experiment", dest="experiment", help="Experiment name")
     optparser.add_option("-p", "--project", dest="project", help="Project name")
-    optparser.add_option("-b", "--base", dest="base", help="Path and name of the experiment config file")
-    optparser.add_option("-a", "--agent", dest="agent", help="Agent IDL in the experiment")
-    optparser.add_option("-l", "--aal", dest="aal", help="AAL file of the experiment procedure")
-    optparser.add_option("-T", "--Tunnel", dest="tunnel", default=False, help="Tunnel request through Deter Ops (users.deterlab.net).")
+    optparser.add_option("-x", "--experimentConfig", dest="experimentConfig", help="Experiment configuration file")
+    optparser.add_option("-c", "--config", dest="config", help="Graph configuration file")
+    optparser.add_option("-a", "--agent", dest="agent", help="Agent name. This is used to fetch available database fields")
+    optparser.add_option("-l", "--aal", dest="aal", help="AAL (experiment procedure) file. This is also used to fetch available database fields")
+    optparser.add_option("-o", "--output", dest="output", default='graph.png', help="Output graph file. Default: %default")
+    optparser.add_option("-t", "--tunnel", dest="tunnel", action="store_true", default=False, help="Tell the tool to tunnel request through Deter Ops (users.deterlab.net).")
     optparser.add_option("-u", "--username", dest="username", help="Username for creating tunnel. Required only if different from current shell username.")
-    optparser.add_option("-c", "--config", dest="config", help="Path and name of configuration file for generating the graph")
-    optparser.add_option("-o", "--output", dest="output", help="Path and name of output file for the graph") 
     (options, args) = optparser.parse_args()
     
     logging.basicConfig(format=helpers.LOG_FORMAT_MSECS, datefmt=helpers.LOG_DATEFMT, level=logging.INFO)
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     if options.agent:
         if options.aal:
             logging.info("Attempting to load the Agent IDL file")
-            agentidl =  helpers.loadIDL(options.agent,options.aal)
+            agentidl =  helpers.loadIDL(options.agent, options.aal)
             #logging.info(agentidl['dbfields'])
             logging.info("Displaying field names")
             print
@@ -43,34 +43,43 @@ if __name__ == '__main__':
             raise RuntimeError, 'Missing AAL file. Please provide AAL file with option -l'
             sys.exit(2)
     else:                            
-     
         if options.config is None:
             optparser.print_help()
             sys.exit(2)
      
-        if options.output is None:
-            optparser.print_help()
-            sys.exit(2)
-    
         logging.info("Attempting to get the database config host from the experiment")
-        dbConfigNode = helpers.getDBConfigHost(experimentConfigFile=options.base, project=options.project, experiment=options.experiment)
+        dbConfigNode = helpers.getDBConfigHost(experimentConfigFile=options.experimentConfig, 
+                                               project=options.project, 
+                                               experiment=options.experiment)
         #logging.info(dbConfigNode)
         logging.info("Got the database config host from the experiment")                                  
     
-        logging.info("Attempting to load the Yaml file")
+        logging.info("Attempting to load the graph configuration file")
         config = helpers.loadYaml(options.config)
-        logging.info("Loaded Yaml file")
+        logging.info("Graph configuration loaded")
 
         if not config.has_key('db') :
             raise RuntimeError, 'Configuration file incomplete. Database options are missing. Use option -a to get fields'
             sys.exit(2)
-        else:
-            if not config['db'].has_key('filter'):
-                raise RuntimeError, 'Configuration file incomplete. Filter options are missing.Use option -a to get fields'
-                sys.exit(2)
         
         #logging.info(config)
+        
+        dbConfig = config['db']
+        graphConfig = config.get('graph', {})
    
+        try:
+            agentName = dbConfig['agent']
+            dataFilter = dbConfig.get('filter', {})
+            xValue = dbConfig['xValue']
+            yValue = dbConfig['yValue']
+            graphType = graphConfig.get('type', 'line')
+            xLabel = graphConfig.get('xLabel', xValue)
+            yLabel = graphConfig.get('yLabel', yValue)
+            graphTitle = graphConfig.get('title', 'Graph')
+        except KeyError:
+            logging.exception("Invalid graph configuration")
+            sys.exit(2)
+            
         try:
             tunnel_cmd = None		
             if options.tunnel:
@@ -85,8 +94,6 @@ if __name__ == '__main__':
                 bridge = dbConfigNode
                 port = 27017
             
-            agentName = config['db']['agent']
-            
             try:
                 logging.info('Attempting to connect to the database')
                 collection = database.getCollection(agentName, bridge, port)
@@ -94,27 +101,27 @@ if __name__ == '__main__':
             except RuntimeError as e:
                 logging.critical("Failed connecting to the database : %s", str(e))
                 sys.exit(2)
-
+            
             x=[]
             y=[]
-            logging.info('The filter applied for data collected: %s',config['db']['filter'])	
-            for firstvalue in collection.findAll(config['db']['filter']).sort("_id", 1)[:1]:
-                logging.info('The first timestamp in database: %s',firstvalue[config['db']['xValue']])
+            logging.info('The filter applied for data collected: %s', dataFilter)
+            firstRecord = collection.findAll(dataFilter).sort(xValue, 1).limit(1)[0]
+            logging.info('The first timestamp in database: %s', firstRecord[xValue])
 
             logging.info('Fetching data from database')
-            for post in collection.find(config['db']['filter']).sort("_id", 1):
-                x.append("%.8f" % (post[config['db']['xValue']] - firstvalue[config['db']['xValue']]))
-                y.append(post[config['db']['yValue']])
+            for record in collection.findAll(dataFilter).sort(xValue, 1):
+                x.append("%.8f" % (record[xValue] - firstRecord[xValue]))
+                y.append(record[yValue])
           
             logging.info('Constructed the x and y values for graph')
   
             """ Check for type of graph needed and print """          
             
-            if config['graph']['type'] == 'line':
-                visualization.line_Graph(config['graph']['xLabel'],config['graph']['yLabel'],config['graph']['title'],x,y,options.output)
-            elif config['graph']['type'] == 'scatter':
-                visualization.scatter_Graph(config['graph']['xLabel'],config['graph']['yLabel'],config['graph']['title'],x,y,options.output)
-            logging.info('Printed and saved the graph')	    
+            if graphType == 'line':
+                visualization.line_Graph(xLabel, yLabel, graphTitle, x, y, options.output)
+            elif graphType == 'scatter':
+                visualization.scatter_Graph(xLabel, yLabel, graphTitle, x, y, options.output)
+            logging.info('Saved graph at %s' %(options.output))	    
 
         finally:
             if tunnel_cmd:
