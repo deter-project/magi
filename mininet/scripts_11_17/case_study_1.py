7a9d8b1ca65c9bc1542f482305a274c738509e0d
koubagade <bagade@usc.edu>
2014-11-19 16:20:46 -0800

    updated case study 1 scripts

--- mininet/scripts_11_17/case_study_1.py
+++ mininet/scripts_11_17/case_study_1.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python
-"""Custom topology example
-
-Two directly connected switches plus a host for each switch:
-
-   host --- switch --- switch --- host
-
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
-"""
 
 from mininet.topo import Topo
 from mininet.net import Mininet
@@ -20,6 +11,9 @@ import os
 import time
 import optparse
 import logging
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
 
 class MyTopo( Topo ):
     "Simple topology example."
@@ -36,7 +30,7 @@ class MyTopo( Topo ):
         if not os.path.exists(directory):
             os.makedirs(directory)
             os.makedirs("%s/tmp" % directory)
-	ip = h1.IP()
+	
         rightHost = self.addHost( 'h2' )
         directory = "/tmp/h2"
         if not os.path.exists(directory):
@@ -46,11 +40,11 @@ class MyTopo( Topo ):
         # Add links
         self.addLink( leftHost, rightHost )
         
-def perfTest(aalfile="cs_procedure.aal"):
+def perfTest(aalfile='cs_procedure.aal'):
 	topo = MyTopo()
 	net = Mininet(topo=topo, host=CPULimitedHost, link=TCLink)
 	net.start()
-	
+	log.info("aal file is %s " % aalfile)
 	dumpNodeConnections(net.hosts)
 	h1, h2 = net.getNodeByName('h1', 'h2')
 	log.info( "running daemon on h1")
@@ -60,8 +54,12 @@ def perfTest(aalfile="cs_procedure.aal"):
 	log.info("running daemon on h2")
 	#h2.cmd("hostname h2")
 	h2.cmd('~/magi/scripts/magi_daemon.py --nodeconf ~/h2.conf -l DEBUG &')
+	time.sleep(20)	
 	log.info("running orchestrator on h1")
-	h1.cmd('~/magi/tools/magi_orchestrator.py --events ' + aalfile + " -b 10.0.0.1')
+	h1.cmd('~/magi/tools/magi_orchestrator.py --events %s -b 10.0.0.1' % aalfile)
+	log.info("plotting graph using magi visualtization tool")
+	h1.cmd('~/magi/tools/magi_graph.py -d 10.0.0.1 -r 27018 -c graph.conf -o graph.png')
+	log.info("graph is stored in graph.png")
 	CLI(net)
 	log.info( "stopping")
 	net.stop()
@@ -74,6 +72,6 @@ if __name__ == '__main__':
 	if not options.aal :
 		optparser.print_help()
 		optparser.error("Missing AAL file")
-	
-	setLogLevel('options.loglevel')
-	perfTest(options.aal)
+	log.info("aal file is %s " % options.aal)
+	setLogLevel(options.loglevel)
+	perfTest(aalfile=options.aal)
