6e410105ef09562c3a395a3141a0993b0095ceae
Prateek Jaipuria <jaipuria@isi.edu>
2015-01-17 19:10:36 -0800

    mininet bootstrap tools

---
+++ tools/mininet_bootstrap.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python
+
+import logging
+import logging.handlers
+from optparse import OptionParser
+import os
+import sys
+import time
+
+from networkx.readwrite import json_graph
+import yaml
+
+from helpers import call, is_os_64bit, is_running, verifyPythonDevel
+from helpers import isInstalled, installPython, installC, installPackage, installPreBuilt
+import networkx as nx
+
+class BootstrapException(Exception): pass
+class TarException(BootstrapException): pass
+class PBuildException(BootstrapException): pass
+class CBuildException(BootstrapException): pass
+class PackageInstallException(BootstrapException): pass
+class DException(BootstrapException): pass
+
+log = logging.getLogger()
+
+if __name__ == '__main__':
+	
+	usage = "usage: sudo python networkx_parser.py -f <topology filename>"
+	parser = OptionParser()
+	parser.add_option("-f", "--file", dest="file", help="json topology FILE")
+	parser.add_option("-p", "--distpath", dest="rpath", default="/share/magi/current", help="Location of the distribution") 
+	parser.add_option("-U", "--noupdate", dest="noupdate", action="store_true", default=False, help="Do not update the system")
+	parser.add_option("-N", "--noinstall", dest="noinstall", action="store_true", default=False, help="Do not install supporting libraries") 
+	parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False, help="Include debugging information") 
+	parser.add_option("-o", "--logfile", dest="logfile", action='store', help="Log file. Default: %default")
+
+	(options, args) = parser.parse_args()
+	
+	log_format = '%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s %(message)s'
+	log_datefmt = '%m-%d %H:%M:%S'
+	
+	log = logging.getLogger()
+	log.handlers = []
+
+	if options.logfile:
+		# Roll over the old log and create a new one
+		# Note here that we will have at most 5 logs 
+		# Need to check existence of file before creating the handler instance
+		# This is because handler creation creates the file if not existent 
+		if os.path.isfile(options.logfile):
+			needroll = True
+		else:
+			needroll = False
+		handler = logging.handlers.RotatingFileHandler(options.logfile, backupCount=5)
+		if needroll:
+			handler.doRollover()
+	
+	else:
+		handler = logging.StreamHandler()
+
+	handler.setFormatter(logging.Formatter(log_format, log_datefmt))
+	log.setLevel(logging.DEBUG)
+	log.addHandler(handler)
+
+	log = logging.getLogger(__name__)
+	#log.info('set log level to %s (%d)' % (options.loglevel, log.getEffectiveLevel()))
+
+	if options.file:
+		jgraph = yaml.load(open(options.file))
+		topoGraph = json_graph.node_link_graph(jgraph)
+# 		G = nx.Graph()
+# 		G.add_nodes_from(['h1','h2','h3','h4', 'h5', 'h6'])
+# 		G.add_edge('h1','h2')
+# 		G.add_edge('h1','h3')
+# 		G.add_edge('h1','h4')
+# 		G.add_edge('h2','h5')
+# 		G.add_edge('h5','h6')	
+# 		print "Nodes %s " % G.nodes()
+# 		print "Edges %s"  % G.edges()
+	else:
+		parser.print_help()
+		parser.error("Missing topology file")
+		
+	log.info("Topology Graph: %s" %(topoGraph))
+	
+	rpath = options.rpath
+	
+	try:
+	
+		if (not options.noupdate) and (not options.noinstall):  # double negative
+				if isInstalled('yum'):
+						call("yum update")
+				elif isInstalled('apt-get'):
+						call("apt-get -y update")
+				else:
+						msg = 'I do not know how to update this system. Platform not supported. Run with --noupdate or on a supported platform (yum or apt-get enabled).'
+						log.critical(msg)
+						sys.exit(msg)  # write msg and exit with status 1
+								
+		verifyPythonDevel()
+	
+		if not options.noinstall:				 
+	
+			try:
+				installPython('PyYAML', 'yaml', 'install', rpath)
+			except PBuildException:
+				installPython('PyYAML', 'yaml', '--without-libyaml install', rpath)  # try without libyaml if build error
+	
+			installPython('unittest2', 'unittest2', 'install', rpath)
+			installPython('networkx', 'networkx', 'install', rpath)
+			
+			magidist = 'MAGI-1.6.0'
+			installPython(magidist, 'alwaysinstall', 'install', rpath)
+			
+			installPackage(yum_pkg_name="python-setuptools", apt_pkg_name="python-setuptools")
+			installPython('pymongo', 'pymongo', 'install', rpath)
+			
+			installPackage(yum_pkg_name="mininet", apt_pkg_name="mininet")
+			installPackage(yum_pkg_name="xterm", apt_pkg_name="xterm")
+			
+			if is_os_64bit():
+				installPreBuilt('mongodb-linux-x86_64', rpath)
+			else:
+				installPreBuilt('mongodb-linux-i686', rpath)
+			
+			#updating sys.path with the installed packages
+			import site
+			site.main()
+	
+		# Now that installation in done on the local node, import utilities 
+		from magi import __version__
+		from magi.util import config, helpers
+		from mininet_helpers import createMininetHosts, Mininet, dumpNodeConnections, CLI
+		
+		topo = createMininetHosts(topoGraph)
+		net = Mininet(topo=topo, controller=None)
+		net.start()
+		dumpNodeConnections(net.hosts)
+	
+		#computes shortest path between all pairs of nodes and updates routing table on each node
+		for srcNode in topoGraph.nodes():
+			log.info("Computing routes for node: %s" %(srcNode))
+			for destNode in topoGraph.nodes():
+				if srcNode != destNode: 		
+					sp = nx.shortest_path(topoGraph, srcNode, destNode)
+					log.info("Route from %s to %s is %s" %(srcNode, destNode, sp))
+					print net[srcNode].cmd('route add -host %s gw %s-%s' %(destNode, sp[1], srcNode))
+			log.info("Routing table for node: %s" %(srcNode))
+			log.info(net[destNode].cmd('route'))
+			
+		hosts = net.hosts
+		for node in hosts:
+			print node.name, node.IP()	
+		
+		print topoGraph.nodes()
+		
+		for nodeName in topoGraph.nodes():
+			node = net.getNodeByName(nodeName)
+			
+			log.info("Starting daemon on %s" %(nodeName))
+			
+			daemonCmd = '/usr/local/bin/magi_daemon.py --nodeconf /tmp/%s/%s.conf' %(nodeName, nodeName)
+
+			if options.verbose:
+				log.info("Starting daemon with debugging")
+				daemonCmd += ' -l DEBUG'
+
+			daemonCmd += " &"
+			
+			log.info(daemonCmd)
+			
+			log.info("MAGI Version: %s", __version__) 
+
+			node.cmd(daemonCmd)
+			
+			time.sleep(1)
+			
+		log.info("Started all daemons and loaded configs")
+		
+		#net.pingAll()
+		CLI(net)
+		net.stop()	
+		
+	except Exception, e:
+		log.exception("Exception while bootstraping")
+		sys.exit(e)
+
+
---
+++ tools/mininet_helpers.py
@@ -0,0 +1,158 @@
+#!/usr/bin/python
+
+from collections import defaultdict
+from collections import deque
+import logging
+import os
+
+from magi.util import helpers
+import yaml
+
+from mininet.cli import CLI
+from mininet.net import Mininet
+from mininet.node import Node
+from mininet.topo import Topo
+from mininet.util import dumpNodeConnections
+
+
+log = logging.getLogger()
+
+iplist = deque([])
+
+for i in range (20):
+	ip = '10.0.%s.1/24'%i
+	iplist.append(ip)
+	ip = '10.0.%s.2/24'%i
+	iplist.append(ip)
+
+ETC_HOSTS_BEGIN = "#--MININET CONFIGURATION BEGIN--#"
+ETC_HOSTS_END = "#--MININET CONFIGURATION END--#"
+
+def clearEtcHostsMininetConfig():
+	f = open("/etc/hosts", "r")
+	lines = f.readlines()
+	f.close()
+
+	f = open("/etc/hosts", "w")
+	skip = False
+	for line in lines:
+		if not skip:
+			if line.rstrip() == ETC_HOSTS_BEGIN:
+				skip = True
+				continue
+			f.write(line)
+
+		elif line.rstrip() == ETC_HOSTS_END:
+			skip = False
+
+	f.close()
+	
+def addEtcHostsMininetConfig(configEntries):
+	clearEtcHostsMininetConfig()
+	f = open("/etc/hosts", "a")
+	f.write("%s\n"%(ETC_HOSTS_BEGIN))
+	for entry in configEntries:
+		f.write(entry)
+		f.write("\n")
+	f.write("%s\n"%(ETC_HOSTS_END))
+	f.close()
+
+'''
+	Enables forwarding in nodes
+'''
+class LinuxRouter(Node):
+	"A Node with IP forwarding enabled."
+	
+	def config(self, **params):
+		super(LinuxRouter, self).config(**params)
+		# Enable forwarding on the router
+		self.cmd('sysctl net.ipv4.ip_forward=1')
+	
+	def terminate(self):
+		self.cmd('sysctl net.ipv4.ip_forward=0')
+		super(LinuxRouter, self).terminate()
+
+class createMininetHosts(Topo):
+	
+	def __init__(self, topoGraph):
+		Topo.__init__(self)
+		interfacelist = defaultdict(list)
+
+		#Creates mininet hosts that can route packets and generates list of interfaces necessary for each host
+		for nodeName in topoGraph.nodes():
+			self.addNode(name=nodeName, cls=LinuxRouter, ip='')
+			neighbors = topoGraph.neighbors(nodeName)
+			intflist = deque([])
+			for i in range(len(neighbors)):
+				intfname = '%s-eth%s'%(nodeName, i)				
+				intflist.append(intfname)
+			interfacelist[nodeName].append(intflist)
+			
+			# create config files for nodes
+			directory = "/tmp/%s" % nodeName
+			if not os.path.exists(directory):
+				os.makedirs(directory)
+				os.makedirs("%s/tmp" % directory)
+			
+			localInfo = dict()
+			localInfo['configDir'] = "/tmp/%s/config" %(nodeName)
+			localInfo['logDir'] = "/tmp/%s/logs" %(nodeName)
+			localInfo['dbDir'] = "/tmp/%s/db" %(nodeName)
+			localInfo['tempDir'] = "/tmp/%s/tmp" %(nodeName)
+			localInfo['nodename'] = nodeName
+			
+			helpers.makeDir(localInfo['configDir'])
+			
+			dbConfig = dict()
+			dbConfig['isDBEnabled'] = True
+			dbConfig['isDBSharded'] = False
+			dbConfig['sensorToCollectorMap'] = {nodeName: nodeName}
+			
+			transportsConfig = []
+			if nodeName != 'server':
+				transportsConfig.append({'class': 'TCPTransport', 'address': 'server', 'port': 28808})
+			else:
+				transportsConfig.append({'class': 'TCPServer', 'address': '0.0.0.0', 'port': 18808})
+				transportsConfig.append({'class': 'TCPServer', 'address': '0.0.0.0', 'port': 28808})
+				
+			nodeConfig = dict()
+			nodeConfig['localInfo'] = localInfo
+			nodeConfig['database'] = dbConfig
+			nodeConfig['transports'] = transportsConfig
+			
+			nodeConfFile = "/tmp/%s/%s.conf" %(nodeName, nodeName)
+			fp = open(nodeConfFile, 'w')
+			fp.write(yaml.safe_dump(nodeConfig))
+			fp.close()
+
+			
+		print "final %s" % interfacelist.items() 
+			
+		#adds edges in mininet topology and configures Ip addresses
+		hostsConfigEntries = []
+		for e in topoGraph.edges():
+			#node1, node2 = self.get(e[0]), self.get(e[1]) 
+			a = []
+			a.append(e[0])
+			a.append(e[1])
+			a.sort()
+			print "adding link between %s and %s" %(a[0], a[1])
+			intf1name = interfacelist[a[0]]
+			intf2name = interfacelist[a[1]]			
+			print "intf1 name = %s,intf1 name = %s" %(intf1name[0], intf2name[0])
+			interface1 = intf1name[0].popleft()
+			interface2 = intf2name[0].popleft()
+			interfacelist[a[0]]=[intf1name[0]]
+			interfacelist[a[1]]=[intf2name[0]]
+			ip1 = iplist.popleft()
+			ip2 = iplist.popleft()
+			#link1 = Link.__init__(e[0], e[1], intfName1=interface1, intfName2=interface2, addr1=iplist.popleft(), addr2=iplist.popleft())
+			self.addLink( a[0], a[1], intfName1=interface1, intfName2=interface2, params1={'ip': ip1 }, params2={ 'ip' : ip2 } )
+			hostsConfigEntries.append("%s	%s-%s %s\n" %(ip1.split('/')[0], a[0], a[1], a[0]))
+			hostsConfigEntries.append("%s	%s-%s %s\n" %(ip2.split('/')[0], a[1], a[0], a[1]))
+			print "\n\nadded link between %s and %s\n\n" %(a[0], a[1])
+			#print self.linkInfo(a[0], a[1])
+			
+		addEtcHostsMininetConfig(hostsConfigEntries)
+				
+
